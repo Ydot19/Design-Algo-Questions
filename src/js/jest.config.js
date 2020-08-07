a95e1123844ee552b760e1cd5249a925384ae04e
Yaadata Abdalhalim <abdalhalim.yaadata@gmail.com>
2020-08-07 14:58:09 -0400

    enhancement: added jest and the config for testing as well as adding nvmrc file for node version management across devices

---
+++ src/js/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    rootDir: __dirname,
+    coverageThreshold: {
+        global: {
+            "function": 100,
+            "branches": 100,
+            "lines": 100,
+            "statements": 90
+        }
+    },
+    coverageReporters: ["lcov", "html"],
+    collectCoverageFrom: [
+        "<rootDir>/core/**/*.js"
+    ],
+    coverageDirectory: "coverage",
+    watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+    moduleFileExtensions: ['js', 'json'],
+    testMatch: ["<rootDir>/test/**/*.test.js"]
+}
